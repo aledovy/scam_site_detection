f517a72f6547ab181ce59703e1777619892cacfb
aledovy <aledovy@gmail.com>
2026-04-27 21:23:47 +0300

    adding config.core

---
+++ core/config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+VT_API_KEY = os.getenv("")
+WHOIS_API_KEY = os.getenv("")
+URLSCAN_API_KEY = os.getenv("")
+
+INPUT_FILE = "C:\Users\dovyd\OneDrive\Documents\Stalinis kompiuteris\Automation\EPERIMENTS\scam_site_detection\input\sample.txt"
+OUTPUT_FILDER = "C:\Users\dovyd\OneDrive\Documents\Stalinis kompiuteris\Automation\EPERIMENTS\scam_site_detection\output"
+
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-
+from core.config import INPUT_FILE, OUTPUT_FILDER
+from logging import log
 
 def main():
     print("this is entry point")
+    with open(INPUT_FILE, "r") as f, open(OUTPUT_FILDER + "_output.csv", "w", newline="") as csvfile:
+        for unclean_domain in list:
+            unclean_domain = unclean_domain.strip()
 
 
 if __name__ == "__main__":
